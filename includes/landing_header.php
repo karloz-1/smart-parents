337142d58587b345bdd5d2dbff4c4590d59165b5
Carlos Gonzalez <d.391@outlook.com>
2025-07-17 18:11:21 -0500

    cambios minimos

---
+++ includes/landing_header.php
@@ -0,0 +1,13 @@
+<?php
+    echo
+    '<header class="header">
+        <div class="header_logo"><a href="index.php">Smart Parents</a></div>
+        <nav class="nav">
+            <ul class="nav_list">
+                <li><a href="index.php">Incio</a></li>
+                <li><a href="views/informacion.html">información</a></li>
+            </ul>
+        </nav>
+        <div class="nav_login"><a href="#">Iniciar Sesión</a></div>
+    </header>'
+?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Smart Parents</title>
+    <link rel="stylesheet" href="assets/css/style.css"> <!-- Conecta el CSS-->
+    <link rel="preconnect" href="https://fonts.googleapis.com"> <!-- Conecta la fuente -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> <!-- Conecta la fuente -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> <!-- Conecta la fuente -->
+</head>
+<body>
+    <?php include 'includes/landing_header.php'?> <!-- Incluye el header -->
+    <main class="main">
+        <section class="main_content">
+            <div>
+                <h1 class="main_title">SMART PARENTS</h1>
+                <p class="main_text">
+                    SMART PARENTS es una plataforma web institucional desarrollada en HTML, CSS, PHP y MySQL, cuyo objetivo principal es mantener informados a los padres de familia sobre la situación académica y comportamental de sus hijos. Esta plataforma permitirá a los usuarios acceder a información de manera clara, rápida y segura.
+                </p>
+                <a href="#" class="main_login_btn">Iniciar Sesión</a>
+            </div>
+        </section>
+        <section>
+            <img src="assets/img/id_card.svg" alt="ilustracion de tarjeta">
+        </section>
+    </main>
+</body>
+</html>
