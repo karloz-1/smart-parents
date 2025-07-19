e19c3769c8daa432884e19460c7cdfb42654f778
Carlos Gonzalez <d.391@outlook.com>
2025-07-18 23:25:26 -0500

    landing page reconstruido

--- index.php
+++ index.php
@@ -10,20 +10,18 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> <!-- Conecta la fuente -->
 </head>
 <body>
-    <?php include 'includes/landing_header.php'?> <!-- Incluye el header -->
+    <?php include 'includes/landing_header.php'?>
     <main class="main">
-        <section class="main_content">
-            <div>
-                <h1 class="main_title">SMART PARENTS</h1>
-                <p class="main_text">
+        <div class="main_card">
+            <div class="card_content">
+                <h1 class="card_title">SMART PARENTS</h1>
+                <p class="card_text">
                     SMART PARENTS es una plataforma web institucional desarrollada en HTML, CSS, PHP y MySQL, cuyo objetivo principal es mantener informados a los padres de familia sobre la situación académica y comportamental de sus hijos. Esta plataforma permitirá a los usuarios acceder a información de manera clara, rápida y segura.
                 </p>
                 <a href="#" class="main_login_btn">Iniciar Sesión</a>
             </div>
-        </section>
-        <section>
-            <img src="assets/img/id_card.svg" alt="ilustracion de tarjeta">
-        </section>
+        </div>
+        <img class="id_card" src="assets/img/id_card.svg" alt="ilustracion de tarjeta">
     </main>
 </body>
 </html>
