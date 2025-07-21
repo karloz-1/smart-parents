885d10de69469215f9cb0b51eacd0780aa428dff
Carlos Gonzalez <d.391@outlook.com>
2025-07-21 17:20:13 -0500

    preparacion de la base de datos

--- sql/db.sql
+++ sql/db.sql
@@ -4,5 +4,17 @@ USE smart_parents;
 
 CREATE TABLE usuarios (
     id_usuario INT AUTO_INCREMENT PRIMARY KEY,
-    rol ENUM('estudiante', 'pad')
+    rol ENUM('estudiante', 'profesor', 'administrador'),
+    identificacion VARCHAR(10) UNIQUE,
+    nombre1 VARCHAR(20),
+    nombre2 VARCHAR(20) NULL,
+    apellido1 VARCHAR(20),
+    apellido2 VARCHAR(20) NULL,
+    email VARCHAR(100) UNIQUE,
+    telefono VARCHAR (15),
+    password VARCHAR(255),
+    cargo VARCHAR(20) NULL,
+    asignatura VARCHAR(20) NULL,
 )
+
+INSERT INTO usuarios (rl)
