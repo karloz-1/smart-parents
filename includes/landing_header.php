b06016a811c2591d4479af165e42cd40f99a6a2f
Carlos Gonzalez <d.391@outlook.com>
2025-07-25 19:14:24 -0500

    Mejoras en la cosistencia de los estilos. y codigo comentado

--- includes/landing_header.php
+++ includes/landing_header.php
@@ -1,13 +1,17 @@
 <?php
     echo
-    '<header class="header">
-        <div class="header_logo"><a href="index.php">Smart Parents</a></div>
-        <nav class="nav">
-            <ul class="nav_list">
-                <li><a href="index.php">Incio</a></li>
-                <li><a href="views/informacion.html">información</a></li>
+    '<header class="header">    <!-- Etiqueta de encabezado -->
+        <div class="header_logo">   <!-- Contenedor del "logo" en este caso es el texto que está a la izquierda del encabezado -->
+            <a href="index.php">Smart Parents</a>   <!-- Botón que lleva al incio -->
+        </div>
+        <nav class="header_nav">    <!-- Contenedor de navegación del sitio -->
+            <ul class="header_nav_list">    <!-- Lista de enlaces del menú de navegación -->
+                <li><a href="index.php">Inicio</a></li>                     <!-- Enlace a al inicio de la pagina -->
+                <li><a href="views/informacion.html">información</a></li>   <!-- Enlace a la pagina de información-->
             </ul>
         </nav>
-        <div class="nav_login"><a href="views/login.php">Iniciar Sesión</a></div>
+        <div class="header_nav_login">  <!-- Contenedor del boton de iniciar sesión -->
+            <a href="views/login.php" class="btn_secundary">Iniciar Sesión</a>  <!-- Botón para ir a la pagina de iniciar sesión -->
+        </div>
     </header>'
 ?>
