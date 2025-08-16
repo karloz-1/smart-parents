d2a0dc1f79d85f4204f0c6b2bad7e368e0900fe2
Carlos Gonzalez <d.391@outlook.com>
2025-08-16 03:13:21 -0500

    se mejoró la consulta para mostrar todos los datos de los usuarios

--- actions/usuarios/crud_usuarios.php
+++ actions/usuarios/crud_usuarios.php
@@ -1,15 +1,9 @@
-<?php 
-    session_start();
-    // Verifica que el usuario sea administrador
-    if (!isset($_SESSION['rol']) || $_SESSION['rol'] != 'administrador') {
-        echo "Acceso denegado.";
-        exit();
-    }
-
+<?php
     // Conexion a la base de datos
     include $_SERVER['DOCUMENT_ROOT'] . '/smart-parents/config/db_config.php';
 
-    // Consulta
-    $sql = "SELECT * FROM usuarios ORDER BY id_usuario DESC";
-    $resultado = $conexion->query($sql);
+    // Consulta SQL para mostrar todos los usuarios
+    $stmt = $conexion->prepare("SELECT * FROM usuarios ORDER BY id_usuario");
+    $stmt->execute();
+    $resultado = $stmt->get_result();
 ?>
