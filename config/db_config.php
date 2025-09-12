b5aabade63d24c68503136764adef1f0baf05f33
Carlos G. <faxingberlin08@gmail.com>
2025-09-12 16:32:34 -0500

    utf8 para la conexion a la base de datos.
    Con esto la conexion a la base de datos permite que se puedan comunicar correctamente sin errores en caracteres especiales como la ñ o tildes, especialmente en el hosting

--- config/db_config.php
+++ config/db_config.php
@@ -6,6 +6,7 @@
     $dbname = "smart_parents";
 
     $conexion = new mysqli($dbhost,$dbuser,$dbpass,$dbname,3306);
+    $conexion -> set_charset("utf8");
 
     if ($conexion->connect_error){
         die("Fallo en la conexion".$conexion->connect_error);
