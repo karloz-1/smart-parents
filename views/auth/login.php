b42bfa2322eb06ac665c75321c13ae070a7f03d0
Carlos G. <faxingberlin08@gmail.com>
2025-09-23 21:57:54 -0500

    todas las paginas tienen el atributo lang en 'es'

--- views/auth/login.php
+++ views/auth/login.php
@@ -7,7 +7,7 @@ if (isset($_SESSION['id_usuario'])) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
